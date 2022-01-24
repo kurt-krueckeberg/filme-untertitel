d2658eef4773cc37f7910dcbaffd7690a83e009a
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-01-24 17:07:40 -0500

    updates

--- build.d
+++ build.d
@@ -8,7 +8,7 @@ import std.regex;
 import std.range;
 
 /*
- * In D, types 'char', 'string', 'char[]' are all built-in types and all are UTF-8 character-encoded characters. wchar is utf-16 and dchar is utf-32. 
+ * In D, types 'char', 'string', 'char[]' are all built-in UTF-8 character-encoded types. wchar is utf-16 and dchar is utf-32. 
  */
 
 void main(string[] args)
@@ -41,6 +41,7 @@ EOS";
    try {
 
       auto ofile = File(args[3], "w");
+
       ofile.write(header);
 
       auto dfile = File(args[1], "r");
@@ -52,25 +53,21 @@ EOS";
 
 	  efile.readln(ebuf); 
 
-	  string par;
+	  string ptag;
 	  int line_start = 0;
 
-	  // '-' imples a new speaker.
-          if (line[0] == '-') {
+          if (line[0] == '-') {// '-' imples a new speaker.
 
-	        par =  "<p class='new-speaker'>" ;
+	        ptag =  "<p class='new-speaker'>" ;
 		line_start = 2;
 
 	  } else {
 
-		par = "<p>"; 
+		ptag = "<p>"; 
 	  }
 
-          //auto par = (line[0] == '-') ?  "<p class='new-speaker'>" : "<p>"; 
-
           // Slice off the newline from ebuf
-  
-          ofile.writeln(par, line[line_start .. $], "</p>\n", par, ebuf[line_start .. ebuf.length - 1], "</p>");
+          ofile.writeln(ptag, line[line_start .. $], "</p>\n", ptag, ebuf[line_start .. ebuf.length - 1], "</p>");
       }
 
       ofile.write(footer);
@@ -84,5 +81,4 @@ EOS";
    } finally {
 
    }
-
 }
