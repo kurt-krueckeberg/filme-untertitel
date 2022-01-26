455881f7f5108ed02030759f98bcf51064107305
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-01-26 17:18:46 -0500

    updates

--- build.d
+++ build.d
@@ -17,8 +17,9 @@ string header = q"EOS
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-        <link rel="stylesheet" href="general-style.css">
+	<meta http-equiv="content-type" content="text/html" charset=utf-8"/>
+        <link rel="stylesheet" type="text/css" media="screen" href="screen.css">
+        <link rel="stylesheet" type="text/css" media="print" href="print.css">
 	<title></title>
 </head>
 <body>
