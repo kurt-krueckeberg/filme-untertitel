290f64aaf53e0a6becf0d17659da02a0d7630297
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2022-03-18 13:46:28 -0400

    update

--- build.d
+++ build.d
@@ -11,16 +11,16 @@ import std.range;
 void main(string[] args)
 {
 string header = q"EOS
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-<head>
-    <meta http-equiv="content-type" content="text/html" charset="UTF-8"/>
-    <link rel="stylesheet" type="text/css" media="screen" href="../screen.css">
-    <link rel="stylesheet" type="text/css" media="print" href="../print.css">
-    <title>German&ndash;English Translation</title>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+      <title>TODO supply a title</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" media="screen" href="../screen.css" />
 </head>
 <body>
-<div id="container">
+<div class="container">
 EOS";
 
 string footer = q"EOS
